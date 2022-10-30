9a46a2752f4358b24e5c02dcc15a99b6732eebcc
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-10-30 23:23:00 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,46 @@
+from os import environ
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import asyncio
+
+C = [".", "/"]
+CHANNELS = []
+for grp in environ.get("CHANNELS").split():
+    CHANNELS.append(int(grp))
+
+authchat = filters.chat(CHANNELS) if CHANNELS else (filters.group | filters.channel)         
+
+User = Client(
+    name = "acceptUser",
+    session_string = environ.get("SESSION"),
+    api_id = int(environ.get("API_ID")),
+    api_hash = environ.get("API_HASH")
+)
+
+@User.on_message(filters.command(["run", "approve"], C) & authchat)                     
+async def approve(client: User, message: Message):
+    chat=message.chat 
+    try:
+       await message.delete()
+       await client.approve_all_chat_join_requests(chat.id) 
+       return await client.send_message(chat.id, "mission completed ✅️ approved all joinrequest")
+       await asyncio.sleep(4)
+       await message.delete()   
+    except Exception as e:
+       print(e)
+
+@User.on_message(filters.command(["no", "remove", "decline"], C) & authchat)                     
+async def decline(client: User, message: Message):
+    chat=message.chat 
+    try:
+       await message.delete()
+       await client.decline_all_chat_join_requests(chat.id) 
+       return await client.send_message(chat.id, "mission completed ❌️ declined all joinrequest")
+       await asyncio.sleep(4)
+       await message.delete()      
+    except Exception as e:
+       print(e)
+
+
+print("bot started....")
+User.run(
