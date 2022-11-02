59a3b4b1ef0bab4a7a66c0097f3744318c7c8b51
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-11-02 07:23:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,21 +20,23 @@ async def approve(client: User, message: Message):
     chat=message.chat 
     try:
        await message.delete()
-       await client.approve_all_chat_join_requests(chat.id) 
-       await client.send_message(chat.id, "✅️ approving all joinrequest 🙏 please wait...")         
+       await client.approve_all_chat_join_requests(chat.id)
+       return        
     except Exception as e:
        print(e)
-
+    await client.send_message(chat.id, "✅️ approving all joinrequest 🙏 please wait...")
+        
 @User.on_message(filters.command(["no", "remove", "decline"], C) & authchat)                     
 async def decline(client: User, message: Message):
     chat=message.chat 
     try:
        await message.delete()
-       await client.decline_all_chat_join_requests(chat.id) 
-       await client.send_message(chat.id, "❌️ declining all joinrequest 🙏 please wait...")            
+       await client.decline_all_chat_join_requests(chat.id)
+       return       
     except Exception as e:
        print(e)
-
+    await client.send_message(chat.id, "❌️ declining all joinrequest 🙏 please wait...")  
+         
 
 print("bot started....")
 User.run()
