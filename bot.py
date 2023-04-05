3536ce3999758b6ed90e6b3ba783a0131efca2cb
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-04-05 20:33:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,58 +1,53 @@
+import loggin, asyncio
+
 from os import environ
-import logging
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
-import asyncio
 
 logging.basicConfig(level=logging.ERROR)
 
-C = [".", "/"]
-
 CHANNELS = [int(CHANNEL) for CHANNEL in environ.get("CHANNELS", None).split()]       
-
 AuthChat = filters.chat(CHANNELS) if CHANNELS else (filters.group | filters.channel)         
+User     = Client(name = "AcceptUser", session_string = environ.get("SESSION"))
 
-User = Client(
-    name = "AcceptUser",
-    session_string = environ.get("SESSION"),
-    api_id = int(environ.get("API_ID")),
-    api_hash = environ.get("API_HASH")
-)
 
-@User.on_message(filters.command(["run", "approve", "start"], C) & AuthChat)                     
+@User.on_message(filters.command(["run", "approve", "start"], [".", "/"]) & AuthChat)                     
 async def approve(client: User, message: Message):
-    chat = message.chat 
+    Id = message.chat.id
+    await message.delete(True)
+ 
     try:
-       try:
-          await client.approve_all_chat_join_requests(chat.id)
-          return
-       except FloodWait as t:
-          asyncio.sleep(t.value)
-          await client.approve_all_chat_join_requests(chat.id)
-          return    
-    except Exception as e:
-        logging.error(str(e))
-    msg = await client.send_message(chat.id, "**Task Completed** ✓ **Approved Pending All Join Request**")
+       While True: # create loop is better techniq 🙃
+           try:
+               await client.approve_all_chat_join_requests(Id)         
+           except FloodWait as t:
+               asyncio.sleep(t.value)
+               await client.approve_all_chat_join_requests(Id) 
+           except Exception as e:
+               logging.error(str(e))
+    except FloodWait as s:
+        asyncio.sleep(s.value)
+        While True:
+           try:
+               await client.approve_all_chat_join_requests(Id)         
+           except FloodWait as t:
+               asyncio.sleep(t.value)
+               await client.approve_all_chat_join_requests(Id) 
+           except Exception as e:
+               logging.error(str(e))
+
+    msg = await client.send_message(Id, "**Task Completed** ✓ **Approved Pending All Join Request**")
     await asyncio.sleep(3)
     await msg.delete()
- 
-@User.on_message(filters.command(["no", "remove", "decline"], C) & AuthChat)                     
-async def decline(client: User, message: Message):
-    chat = message.chat 
-    try:
-       try:
-          await client.decline_all_chat_join_requests(chat.id)
-          return   
-       except FloodWait as t:
-          asyncio.sleep(t.value)
-          await client.decline_all_chat_join_requests(chat.id)
-          return     
-    except Exception as e:
-        logging.error(str(e))
-    msg = await client.send_message(chat.id, "**Task Completed** ✓ **Declined All Join Request**")  
-    await asyncio.sleep(3)
-    await msg.delete()     
+
 
 logging.info("Bot Started....")
 User.run()
+
+
+
+
+
+
+
